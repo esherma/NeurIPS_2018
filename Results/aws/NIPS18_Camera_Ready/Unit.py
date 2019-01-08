6b9b68f06c411c0349521cce04a24c5310e2db20
Eli Sherman <esherma@umich.edu>
2019-01-08 15:05:54 -0500

    Removing extra code from results

--- Results/aws/NIPS18_Camera_Ready/Unit.py
+++
@@ -1,8 +0,0 @@
-class unit():    
-    def __init__(self, C_in, U_in, adj):
-        self.C = C_in
-        self.U = U_in
-        self.A = 0
-        self.M = 0
-        self.Y = 0
-        self.adj = adj
