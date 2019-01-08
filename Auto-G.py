205be1e843cd6801f7c0500e2253e2442802c1ce
Eli Sherman <esherma@umich.edu>
2019-01-08 16:08:43 -0500

    Finalized comments in Auto-G, Estimation files

--- Auto-G.py
+++ Auto-G.py
@@ -11,9 +11,9 @@ from Estimation import getEstimates
 import multiprocessing
 from multiprocessing import Pool
 
-
 nodeList = {}
 
+#Parameters for the experiments for each variable
 Cscale = [[1.5, 3], [6, 2], [.8, .8]]
 Uscale = [[2.3, 1.1], [.9, 1.1], [2, 2]]
 tauA = {'intcp': -1, 'C0': .5, 'C1': .2, 'C2': .25, 'U0': .3, 'U1': -.2, 'U2': .25}
@@ -21,12 +21,12 @@ tauM = {'intcp': -1, 'C0': -.3, 'C1': .4, 'C2': .1, 'A': 1, 'nborA': -.5, 'nborM
 tauY = {'intcp': -.3, 'nborA' : -1, 'M': 3, 'C0': -.2, 'C1': .2, 'C2': -.05, 'U0': .1, 
         'U1': -.2, 'U2': .25}
 
-
-# In[3]:
-
+#Load pre-created graph
 with open('./400_3/graph400_3.pkl', 'rb') as fname:
     graph = pickle.load(fname)
     
+    
+#Calculate maximally independent set of Units in graph
 Xi = []
 lens = []
 for i in range(50):
@@ -34,16 +34,17 @@ for i in range(50):
     lens.append(len(Xi[-1]))
 S_max = Xi[np.argmax(lens)]
 
-
+#Calculate causal effects from graph
 def doInference(i):
     print('starting', i)
     nodeList = generateData(graph)
     return getEstimates(nodeList, S_max)
 
-
+#Use multiprocessing to perform (N) bootstrap samples (make it fast!)
 with Pool(multiprocessing.cpu_count()) as pool:
     Effects = pool.map(doInference, np.arange(1000))
 
+#Dump results to disk
 with open('./400_3/Effects400_3.pkl', 'wb') as fname:
     pickle.dump(Effects, fname)
     
--- Estimation.py
+++ Estimation.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 from networkx.generators.random_graphs import random_regular_graph
 import networkx as nx
 import numpy as np
@@ -15,10 +9,10 @@ import pickle
 import copy
 from DataGen import generateData
 
-
-# In[2]:
-
-
+#Function to estimate the parameters of M via the coding likelihood function
+#Inputs: tauM - parameters for M, S_max - listing of node IDs in maximal independent set,
+#        nodeList - dictionary representation of Units in graph
+#Output: likelihood value associated with tauM
 def codingLikelihoodM(tauM, S_max, nodeList):
     total = 0
     for i in S_max:
@@ -35,6 +29,10 @@ def codingLikelihoodM(tauM, S_max, nodeList):
 
     return (-1 * total)
 
+#Function to estimate the parameters of M via the pseudo likelihood function
+#Inputs: tauM - parameters for M, S_max - listing of node IDs in maximal independent set,
+#        nodeList - dictionary representation of Units in graph
+#Output: likelihood value associated with tauM
 def pseudoLikelihoodM(tauM, nodeList):
     total = 0
     for i in nodeList:
@@ -51,10 +49,10 @@ def pseudoLikelihoodM(tauM, nodeList):
 
     return (-1 * total)
 
-
-# In[3]:
-
-
+#Function for sampling from the joint distribution p(M1, M2, ...)
+#Inputs: numIter - number of iterations to run the sampler, M - a dummy initialization matrix,
+#        tauM - parameters for M, nodeList - dictionary representation of the nodes in the graph (elements are Units; see Unit.py)
+#Output: M - a single sample from the joint p(M1, M2, ...)
 def Gibbs(numIter, M, tauM, nodeList):    
         for k in range(1, numIter+1):
             for i in range(len(nodeList)):
@@ -72,27 +70,26 @@ def Gibbs(numIter, M, tauM, nodeList):
 
         return M
 
-
-# In[4]:
-
-
+#Function to perform an intervention in the network
+#Inputs: nodeList - dictionary representation of Units in graph, aVal - interventional value to set for A for ALL units in network
+#Output: nodeListIntervention - modified nodeList post-intervention (but not accounting for downstream effects)
 def doIntervention(nodeList, aVal):
     nodeListIntervention = copy.deepcopy(nodeList)
     for node in nodeListIntervention:
         nodeListIntervention[node].A = aVal
     return nodeListIntervention
 
-
-# In[5]:
-
-
+#Function to calculate causal effect estimates associated with interventions on A's in the network
+#Inputs: nodeList - dictionary representation of Units in graph, S_max - listing of node IDs in maximal independent set
 def getEstimates(nodeList, S_max):
+    #create initialized matrices for units' variables
     As = np.zeros(len(nodeList))
     nborAs = np.zeros((len(nodeList), len(nodeList[0].adj)))
     Cs = np.zeros((len(nodeList), 3))
     Ms = np.zeros(len(nodeList))
     Ys = np.zeros(len(nodeList))
 
+    #Load data into nice, neat arrays
     for node in nodeList:
         As[node] = nodeList[node].A
         idx = 0
@@ -103,13 +100,16 @@ def getEstimates(nodeList, S_max):
         Ms[node] = nodeList[node].M
         Ys[node] = nodeList[node].Y
 
+    #Fit P(A|C) model
     clfA = LogisticRegression()
     clfA.fit(Cs, As)
 
+    #Fit P(Y|C, A, M) model
     cov = np.concatenate((Cs, np.reshape(As, (As.shape[0], 1)), nborAs, np.reshape(Ms, (Ms.shape[0], 1))), axis=1)
     clfY = LogisticRegression()
     clfY.fit(cov, Ys)
     
+    #Fit tauM via coding and pseudo likelihood estimation
     tauM = np.random.rand(7)
     codingM = minimize(codingLikelihoodM, tauM, (S_max, nodeList)).x
     codingM = {'intcp': codingM[0], 'C0': codingM[1], 'C1': codingM[2], 'C2': codingM[3], 'A': codingM[4],
@@ -119,10 +119,10 @@ def getEstimates(nodeList, S_max):
                'nborA': pseudoM[5], 'nborM': pseudoM[6]}
     
     #[p(0|C), p(1|C)]
-
     A1_NetEffect = doIntervention(nodeList, 1)
     A0_NetEffect = doIntervention(nodeList, 0)
 
+    #Run Gibbs to obtain M's for each intervention
     initMat1 = np.random.binomial(1, .5, (1051, len(nodeList)))
     initMat0 = np.random.binomial(1, .5, (1051, len(nodeList)))
     NetEffectCoding1 = Gibbs(1050, initMat1, codingM, A1_NetEffect)
@@ -147,12 +147,15 @@ def getEstimates(nodeList, S_max):
     Y_cod = 0
     Y_pse = 0
     
+    #Average over all nodes in graph
     for i in range(len(nodeList)):
+        #Predict A given C
         Acov = np.array([nodeList[i].C[0], nodeList[i].C[1], nodeList[i].C[2]])
         Ahat = clfA.predict_proba(Acov.reshape(1,-1))
         Yi_cod = 0
         Yi_pse = 0
         
+        #Predict Y 5 times for each intervention for coding likelihood to account for randomness in Gibbs process
         for j in range(5):
             for k in range(len(A_i_vals)):
                 Y1_cod_cov = np.array([nodeList[i].C[0], nodeList[i].C[1], nodeList[i].C[2], A_i_vals[k], 1, 1, 1, NetEffectCoding1[j, i]])
@@ -160,8 +163,10 @@ def getEstimates(nodeList, S_max):
                 Y0_cod_cov = np.array([nodeList[i].C[0], nodeList[i].C[1], nodeList[i].C[2], A_i_vals[k], 0, 0, 0, NetEffectCoding0[j, i]])
                 Y0_cod_hat = clfY.predict_proba(Y0_cod_cov.reshape(1,-1))
                 Yi_cod += (Y1_cod_hat[0][1] * Ahat[0][k] - Y0_cod_hat[0][1] * Ahat[0][k])
+        #Average over the 5 Gibbs samples for Ms
         Y_cod += Yi_cod / 5
 
+        #Predict Y 5 times for each intervention for pseudo likelihood to account for randomness in Gibbs process
         for j in range(5):
             for k in range(len(A_i_vals)):
                 Y1_pse_cov = np.array([nodeList[i].C[0], nodeList[i].C[1], nodeList[i].C[2], A_i_vals[k], 1, 1, 1, NetEffectPseudo1[j, i]])
@@ -169,8 +174,10 @@ def getEstimates(nodeList, S_max):
                 Y0_pse_cov = np.array([nodeList[i].C[0], nodeList[i].C[1], nodeList[i].C[2], A_i_vals[k], 0, 0, 0, NetEffectPseudo0[j, i]])
                 Y0_pse_hat = clfY.predict_proba(Y0_pse_cov.reshape(1,-1))
                 Yi_pse += (Y1_pse_hat[0][1] * Ahat[0][k] - Y0_pse_hat[0][1] * Ahat[0][k])
+        #Average over the 5 Gibbs samples for Ms
         Y_pse += Yi_pse / 5
 
+    #Average over all nodes in network
     Y_cod = Y_cod / len(nodeList)
     Y_pse = Y_pse / len(nodeList)
     return Y_cod, Y_pse
